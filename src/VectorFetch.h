f7fb10381d902c92dae9b74ddfc29d18344014d0
Kartik Prabhu <kprabhu7@stanford.edu>
2023-01-24 18:13:54 -0800

    Fix vector 0 double precision addressing
    Former-commit-id: d2b3e618e75c2b52b6136daece3813e7f04c8faa

--- src/VectorFetch.h
+++ src/VectorFetch.h
@@ -86,7 +86,6 @@ SC_MODULE(VectorFetchUnit) {
                                         params.addressGen0Loop[1][2] * WIDTH;
 
                   if (params.DP_VEC0) {
-                    K = K * 2;
                     for (int precision = 0; precision < 2; precision++) {
                       int address =
                           static_cast<ac_int<32, false> >((j * K + k) * 2) +
