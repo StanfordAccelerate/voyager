3f662f07bc583ae1da70cc96caaae61df0ab80bb
Kartik Prabhu <kprabhu7@stanford.edu>
2024-03-11 11:44:20 -0700

    Remove exit if command line arguments are supplied
    Former-commit-id: bc10aabb260dc72eb1507288b0556fb04ad27d50

--- test/common/TestRunner.cc
+++ test/common/TestRunner.cc
@@ -9,15 +9,6 @@
 
 extern "C" int sc_main(int argc, char* argv[]) {
   Simulation sim;
-#ifndef SOC_COSIM
-  if (argc > 1) {
-    std::cerr
-        << "ERROR: Don't supply command line arguments, instead use env vars."
-        << std::endl;
-    sim.print_help();
-    std::abort();
-  }
-#endif
   sim.loadMemory();
   sim.run();
   return sim.checkOutput();
