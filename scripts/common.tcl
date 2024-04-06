459eaa090aa23c4abdcc8536fcc5add0e08655af
Kartik Prabhu <kprabhu7@stanford.edu>
2024-04-05 18:22:02 -0700

    Set up build folder structure to use separate directories for each configuration
    Former-commit-id: 0c158c13061daf5ebf3d64d2bea8e9b86502ce18

--- scripts/common.tcl
+++ scripts/common.tcl
@@ -1,9 +1,8 @@
-if { [info exists env(DEBUG)] } {
-  set project_folder ./build/Catapult_debug_$block
-} else {
-  set project_folder ./build/Catapult_$block
-}
+set root [file dirname [file dirname [file normalize [info script] ] ] ]
 
+set clk_period $::env(clock_period)
+
+set project_folder "$root/build/${datatype}_${DIMENSION}x${DIMENSION}/Catapult/clock_${clk_period}/$block"
 if { [file exists $project_folder] } {
   file delete -force -- $project_folder
 }
@@ -13,15 +12,14 @@ project new -dir $project_folder
 project save
 
 options set Project/SolutionName $block
-#options set Output/SubBlockNamePrefix $block
-logfile move ./build/${block}.log
+logfile move "$root/build/${datatype}_${DIMENSION}x${DIMENSION}/Catapult/clock_${clk_period}/$block.log"
 
 options set Message/ErrorOverride ASSERT-1 -remove
 options set Input/TargetPlatform x86_64
 options set /Input/CppStandard c++11
 options set Input/CompilerFlags "-D$datatype -DDIMENSION=$DIMENSION"
 solution options set /Input/CppStandard c++11
-options set Input/SearchPath ./lib
+options set Input/SearchPath "$root/lib"
 options set Output/OutputVHDL false
 options set Architectural/DefaultMemMapThreshold 256
 options set Architectural/DefaultRegisterThreshold 4096
@@ -30,7 +28,7 @@ options set Flows/VCS/SYSC_VERSION 2.3.2
 options set Flows/VCS/VLOGAN_OPTS {+v2k -timescale=1ns/10ps +notimingcheck +define+UNIT_DELAY}
 options set Flows/VCS/VCSSIM_OPTS {+fsdbfile+dump.fsdb +fsdb+all=on +fsdb+dumpon+0}
 options set Flows/VCS/VCS_DOFILE dump.do
-options set Flows/VCS/COMP_FLAGS "-O3 -Wall -Wno-unknown-pragmas -I../../../lib/ -I../../../src/ -I../../../ -DNO_UNIVERSAL -DSIM_$block -D$datatype -DDIMENSION=$DIMENSION"
+options set Flows/VCS/COMP_FLAGS "-O3 -Wall -Wno-unknown-pragmas -I$root/lib/ -I$root/src/ -I$root/ -DNO_UNIVERSAL -DSIM_$block -D$datatype -DDIMENSION=$DIMENSION"
 options set Flows/VCS/VCSELAB_OPTS "-timescale=1ns/1ps -sysc=blocksync -lstdc++fs"
 flow package require /SCVerify
 flow package option set /SCVerify/USE_VCS true
@@ -38,30 +36,29 @@ flow package option set /SCVerify/USE_VCS true
 # set clocks {clk {-CLOCK_PERIOD 5 -CLOCK_EDGE rising -CLOCK_HIGH_TIME 2.5 -CLOCK_OFFSET 0.000000 -CLOCK_UNCERTAINTY 0.0 -RESET_KIND async -RESET_SYNC_NAME rst -RESET_SYNC_ACTIVE high -RESET_ASYNC_NAME arst_n -RESET_ASYNC_ACTIVE low -ENABLE_NAME {} -ENABLE_ACTIVE high}}
 # some blocks require extra margin to avoid Catapult scheduling issues (DC will be able to meet the clock period)
 # other blocks don't
-set clk_period $::env(clock_period)
 set clocks "clk \"-CLOCK_PERIOD $clk_period -CLOCK_EDGE rising -CLOCK_HIGH_TIME [expr $clk_period / 2.0] -CLOCK_OFFSET 0.000000 -CLOCK_UNCERTAINTY 0.0 -RESET_KIND async -RESET_SYNC_NAME rst -RESET_SYNC_ACTIVE high -RESET_ASYNC_NAME arst_n -RESET_ASYNC_ACTIVE low -ENABLE_NAME {} -ENABLE_ACTIVE high\" "
 set clk_period [expr $::env(clock_period) * 2]
 set slower_clock "clk \"-CLOCK_PERIOD $clk_period -CLOCK_EDGE rising -CLOCK_HIGH_TIME [expr $clk_period / 2.0] -CLOCK_OFFSET 0.000000 -CLOCK_UNCERTAINTY 0.0 -RESET_KIND async -RESET_SYNC_NAME rst -RESET_SYNC_ACTIVE high -RESET_ASYNC_NAME arst_n -RESET_ASYNC_ACTIVE low -ENABLE_NAME {} -ENABLE_ACTIVE high\" "
 directive set -CLOCK_OVERHEAD 0
 go new
 
-solution file add ./src/Accelerator.h -type CHEADER
-solution file add ./test/common/TestRunner.cc -type C++ -exclude true
-solution file add ./test/common/Harness.cc -type C++ -exclude true
-solution file add ./test/common/Utils.cc -type C++ -exclude true
-solution file add ./test/common/GoldModel.cc -type C++ -exclude true
-solution file add ./test/common/MemoryModel.cc -type C++ -exclude true
-solution file add ./test/common/SimpleMemoryModel.cc -type C++ -exclude true
-solution file add ./test/common/Simulation.cc -type C++ -exclude true
-solution file add ./test/toolchain/MapOperation.cc -type C++ -exclude true
+solution file add $root/src/Accelerator.h -type CHEADER
+solution file add $root/test/common/TestRunner.cc -type C++ -exclude true
+solution file add $root/test/common/Harness.cc -type C++ -exclude true
+solution file add $root/test/common/Utils.cc -type C++ -exclude true
+solution file add $root/test/common/GoldModel.cc -type C++ -exclude true
+solution file add $root/test/common/MemoryModel.cc -type C++ -exclude true
+solution file add $root/test/common/SimpleMemoryModel.cc -type C++ -exclude true
+solution file add $root/test/common/Simulation.cc -type C++ -exclude true
+solution file add $root/test/toolchain/MapOperation.cc -type C++ -exclude true
 
 # iterate through all files in ./test/toolchain/operations and add them to the solution
-foreach file [glob -nocomplain -directory ./test/toolchain/operations *.cc] {
+foreach file [glob -nocomplain -directory $root/test/toolchain/operations *.cc] {
   solution file add $file -type C++ -exclude true
 }
 
-solution file add ./test/resnet/ResNet.cc -type C++ -exclude true
-solution file add ./test/mobilebert/MobileBERT.cc -type C++ -exclude true
+solution file add $root/test/resnet/ResNet.cc -type C++ -exclude true
+solution file add $root/test/mobilebert/MobileBERT.cc -type C++ -exclude true
 
 
 go analyze
