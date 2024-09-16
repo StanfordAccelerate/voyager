4fa09b1152d7ff0a2737bf0bac7f77b09e6e1045
Kartik Prabhu <kprabhu7@stanford.edu>
2024-09-16 14:21:23 -0700

    Bump quantized-training, copy over test_codegen.py script from quantized-training to run_compiler.py
    Former-commit-id: d0a56a6501033a639ca58d84d9ed075ebc4df8be

--- codegen.mk
+++ codegen.mk
@@ -4,44 +4,44 @@
 ################################################################################
 # ResNet18
 ################################################################################
-test/compiler/networks/resnet18/INT8/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet18/INT8/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet18 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet18 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/resnet18/E4M3/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet18/E4M3/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet18 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet18 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/resnet18/P8_1/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet18/P8_1/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet18 --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet18 --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
 ################################################################################
 # ResNet50
 ################################################################################
-test/compiler/networks/resnet50/INT8/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet50/INT8/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet50 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet50 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/resnet50/E4M3/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet50/E4M3/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet50 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet50 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/resnet50/P8_1/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/resnet50/P8_1/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet50 --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py resnet50 --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
 ################################################################################
 # MobileBERT
 ################################################################################
-test/compiler/networks/mobilebert/INT8/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/mobilebert/INT8/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model mobilebert --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py mobilebert --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/mobilebert/E4M3/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/mobilebert/E4M3/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model mobilebert --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py mobilebert --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
-test/compiler/networks/mobilebert/P8_1/params.pb: quantized-training/test/test_codegen.py
+test/compiler/networks/mobilebert/P8_1/params.pb: test/compiler/run_compiler.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model mobilebert --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+	python test/compiler/run_compiler.py mobilebert --activation posit8_1 --weight posit8_1 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
