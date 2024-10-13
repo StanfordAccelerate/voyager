0e568a90cce15bdf49b12c13790692f9c6e2f3d2
Allen Pan <aifusenno1@gmail.com>
2024-10-12 20:13:44 -0700

    Add buffer size flags in catapult scripts
    Former-commit-id: 705bc2b2be9b8b069904f1b08a104cb26c78abe4

--- scripts/utils/setup_project.tcl
+++ scripts/utils/setup_project.tcl
@@ -14,8 +14,8 @@ logfile move "$ROOT/$CATAPULT_BUILD_DIR/$BLOCK.log"
 solution options set Project/SolutionName $BLOCK
 solution options set Message/ErrorOverride ASSERT-1 -remove
 solution options set Input/TargetPlatform x86_64
-solution options set /Input/CppStandard c++17
-solution options set Input/CompilerFlags "-D$DATATYPE -DIC_DIMENSION=$IC_DIMENSION -DOC_DIMENSION=$OC_DIMENSION"
+solution options set Input/CppStandard c++17
+solution options set Input/CompilerFlags "-D$DATATYPE -DIC_DIMENSION=$IC_DIMENSION -DOC_DIMENSION=$OC_DIMENSION -DINPUT_BUFFER_SIZE=$INPUT_BUFFER_SIZE -DWEIGHT_BUFFER_SIZE=$WEIGHT_BUFFER_SIZE -DACCUM_BUFFER_SIZE=$ACCUM_BUFFER_SIZE"
 solution options set Input/SearchPath "$ROOT/lib"
 solution options set Output/OutputVHDL false
 solution options set Architectural/DefaultMemMapThreshold 256
@@ -25,7 +25,7 @@ solution options set Flows/VCS/SYSC_VERSION 2.3.3
 solution options set Flows/VCS/VLOGAN_OPTS {+v2k -timescale=1ns/10ps +notimingcheck +define+UNIT_DELAY}
 # solution options set Flows/VCS/VCSSIM_OPTS {+fsdbfile+dump.fsdb +fsdb+all=on +fsdb+dumpon+0}
 solution options set Flows/VCS/VCS_DOFILE dump.do
-solution options set Flows/VCS/COMP_FLAGS "-O3 -Wall -Wno-unknown-pragmas -I$ROOT/lib/ -I$ROOT/lib/xtensor/include -I$ROOT/lib/xtl/include -I$ROOT/src/ -I$ROOT/ -I$::env(CONDA_PREFIX)/include -DSIM_$BLOCK -D$DATATYPE -DIC_DIMENSION=$IC_DIMENSION -DOC_DIMENSION=$OC_DIMENSION -std=c++17"
+solution options set Flows/VCS/COMP_FLAGS "-O3 -Wall -Wno-unknown-pragmas -I$ROOT/lib/ -I$ROOT/lib/xtensor/include -I$ROOT/lib/xtl/include -I$ROOT/src/ -I$ROOT/ -I$::env(CONDA_PREFIX)/include -DSIM_$BLOCK -D$DATATYPE -DIC_DIMENSION=$IC_DIMENSION -DOC_DIMENSION=$OC_DIMENSION -DINPUT_BUFFER_SIZE=$INPUT_BUFFER_SIZE -DWEIGHT_BUFFER_SIZE=$WEIGHT_BUFFER_SIZE -DACCUM_BUFFER_SIZE=$ACCUM_BUFFER_SIZE -std=c++17"
 solution options set Flows/VCS/VCSELAB_OPTS "-timescale=1ns/1ps -sysc=blocksync -lstdc++fs -L$::env(CONDA_PREFIX)/lib -LDFLAGS \"-Wl,--enable-new-dtags -Wl,-R,$::env(CONDA_PREFIX)/lib\" -labsl_log_internal_message -labsl_log_internal_check_op -lprotobuf"
 
 flow package require /SCVerify
