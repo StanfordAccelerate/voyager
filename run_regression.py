06bedc62bd13bd30f53f6bdf56e7742d9736f990
Zhouhua Xie <xzh015@caddy09.stanford.edu>
2025-08-10 23:07:06 -0700

    temp change in run_regression compiler call for ci

--- run_regression.py
+++ run_regression.py
@@ -632,8 +632,8 @@ def run_accuracy(model, dataset, num_processes, output_folder):
         subprocess.run(
             [
                 "python",
-                # "quantized-training/test/test_codegen.py",
-                "voyager-compiler/test/test_codegen.py",
+                "quantized-training/test/test_codegen.py",
+                # "voyager-compiler/test/test_codegen.py",
                 model,
                 "--model_name_or_path",
                 model_path,
